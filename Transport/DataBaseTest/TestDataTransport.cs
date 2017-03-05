46ade2acb03c99389eb4ca4bb93cc7a73f0ecd4f
Sergey Mormul <smorkva@gmail.com>
2017-03-05 12:57:33 +0200

    JSON Database (Transport)

---
+++ Transport/DataBaseTest/TestDataTransport.cs
@@ -0,0 +1,27 @@
+﻿using Transport.Shema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataBaseTest
+{
+    [TestClass]
+    public class TestDataTransport
+    {
+        [TestMethod]
+        public void TransportEqualObjectTest()
+        {
+            var transport = new Bus("Some Bus", "ab1234U", 4, 110, "A-B");
+            var copy = transport.Clone();
+            
+            Assert.IsTrue(transport.Equals(copy));
+        }
+        [TestMethod]
+        public void TransportNotEqualChangedObjectTest()
+        {
+            var transport = new Bus("Some Bus", "ab1234U", 4, 110, "A-B");
+            var copy = transport.Clone();
+            (copy as DataTransport).Number = "123";
+
+            Assert.IsFalse(transport.Equals(copy));
+        }
+    }
+}
---
+++ Transport/Transport/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace Transport
+{
+    public sealed class Logger
+    {
+        private static Logger _instance { get; } = new Logger();
+
+        private readonly string _file;
+
+        private Logger()
+        {
+            _file = @"./log.txt";
+        }
+
+        public static void Log(string message)
+        {
+            using (var writer = new StreamWriter(_instance._file, true))
+            {
+                writer.WriteLine($"{DateTime.Now} | {message}");
+            }
+        }
+    }
+}
