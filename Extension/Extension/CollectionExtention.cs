d9c72ef0ecc178ca1965256740fa165871911e5f
Sergey Mormul <smorkva@gmail.com>
2017-03-06 11:00:41 +0200

    Collection extention

---
+++ Extension/Extension/CollectionExtention.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Extension
+{
+    public static class CollencionExtention
+    {
+        static TSource _sumByPredicate<TSource>(this IEnumerable<TSource> collection, Func<TSource, bool> predicate)
+        {
+            var result = default(TSource);
+            foreach (var item in collection)
+            {
+                if (predicate(item))
+                {
+                    result += (dynamic)item;
+                }
+            }
+
+            return result;
+        }
+        public static int SumByPredicate(this IEnumerable<int> collection, Func<int, bool> predicate)
+        {
+            return _sumByPredicate(collection, predicate);
+        }
+        public static long SumByPredicate(this IEnumerable<long> collection, Func<long, bool> predicate)
+        {
+            return _sumByPredicate(collection, predicate);
+        }
+        public static double SumByPredicate(this IEnumerable<double> collection, Func<double, bool> predicate)
+        {
+            return _sumByPredicate(collection, predicate);
+        }
+    }
+}
---
+++ Extension/TestExtension/TestExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Extension;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestExtension
+{
+    [TestClass]
+    public class TestExtension
+    {
+        [TestMethod]
+        public void CollectionIntSum()
+        {
+            var transport = new List<int>() { 1, 2, 3, 4, 5 };
+            var result = transport.SumByPredicate(x => x > 3);
+
+            Assert.AreEqual(9, result);
+        }
+        [TestMethod]
+        public void CollectionDoubleSum()
+        {
+            var transport = new List<double>() { 1.02, 2.486, 3.146, 4.47, 5.145 };
+            var result = transport.SumByPredicate(x => x > 3);
+
+            Assert.AreEqual(12.761, result);
+        }
+        [TestMethod]
+        public void CollectionLongSum()
+        {
+            var transport = new List<long>() { 1, 2, 3, 4, 5 };
+            var result = transport.SumByPredicate(x => x > 3);
+
+            Assert.AreEqual(9, result);
+        }
+    }
+}
